3cff2872629af1c38428848a45c0ea49f8d848cd
LeoEDS <leonardodasilva1015@gmail.com>
2022-05-06 13:50:37 -0300

    Continua

--- Organizador/src/organizador/Organizador.java
+++ Organizador/src/organizador/Organizador.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Organizador {
+        static ArrayList<Materias> listaMaterias = new ArrayList();
+        static ArrayList<Dias> diasMaterias = new ArrayList();
+        static int materiasPorCarpeta;
 
     public static void main(String[] args) {
         Scanner scanf = new Scanner(System.in);
-        ArrayList<Materias> listaMaterias = new ArrayList();
-        ArrayList<Dias> diasMaterias = new ArrayList();
         int i=0;
         String nombre;
 
@@ -23,9 +24,11 @@ public class Organizador {
 
         } while (!nombre.equals("0"));
         
+        int totalPosibilidades[] = new int [listaMaterias.size()]; 
 
         System.out.println("");
         for(i=0; i<5; i++){
+            int x=0;
             System.out.print("Ingresando la Materia de los Dias ");
             switch(i){
                 case 0 : System.out.println(" LUNES... (INGRESE UN 0 PARA TERMINAR)"); break;
@@ -38,20 +41,68 @@ public class Organizador {
             do {
                 System.out.print("Ingrese el Nombre de la Materia:  ");
                 nombre=scanf.nextLine();
-                i++;
+                x++;
                 dia.add(nombre);
             } while (!nombre.equals("0"));
             Dias swap = new Dias(dia);
             diasMaterias.add(swap);
         }
         
+        System.out.print("Ingrese el Numero de Materias por Carpeta: ");
+        materiasPorCarpeta = scanf.nextInt();
         
+        
+        for(i=0;i<listaMaterias.size();i++){
+            totalPosibilidades[i]=i;
+        }
+        System.out.println("Cargando las Posibilades...");
+        helper(totalPosibilidades, 0);
+        for(i=0;i<5;i++) System.out.println();
+        
+        for(i=0;i<posibilidades(totalPosibilidades.length);i++) {
+            for(int x=0;x<totalPosibilidades.length; x++){
+                    if(x%materiasPorCarpeta==0) System.out.println();
+                    System.out.print(v[i][x] + " ");
+            }
+            System.out.println();
+        }
+               
+        
+                
         // ACA VA EL CODIGO DE COMPARACION
         
         
         
         
     }
+    
+    static int v[][] = new int[posibilidades(listaMaterias.size())][listaMaterias.size()];
+    static int tupla=0;
+
+    public static void helper(int[] array, int pos){  
+        if(pos >= array.length - 1){   
+            for(int i = 0; i < array.length-1; i++){  
+                v[tupla][i]=array[i];
+            }  
+            if(array.length > 0)   
+                v[tupla][array.length-1]=array[array.length-1];
+            tupla++;
+            return;  
+        }  
+  
+        for(int i = pos; i < array.length; i++){   
+          
+            int t = array[pos];  
+            array[pos] = array[i];  
+            array[i] = t;  
+  
+            helper(array, pos+1);  
+  
+            t = array[pos];  
+            array[pos] = array[i];  
+            array[i] = t;  
+        }  
+    }  
     
     public static int posibilidades (int posi){
         if(posi<2)
